b16446671bf2757bd5c746d1f464309e629120b6
Olivia <oliviapower.02@gmail.com>
2020-01-20 20:26:21 +0000

    added header view

--- express_server.js
+++ express_server.js
@@ -15,12 +15,11 @@ app.get('/', (req,res)=>{
 
 app.get('/urls', (req,res)=>{
   let templateVars = {urls: urlDatabase};
-  console.log(templateVars);
   res.render('urls_index' , templateVars);
 });
 
 app.get('/urls/:shortURL', (req,res)=>{
-  let templateVars = {shortURL : req.params.shortURL, longURL : req.url};
+  let templateVars = {shortURL : req.params.shortURL, longURL : urlDatabase[req.params.shortURL]};
   res.render('urls_show', templateVars);
 })
 
